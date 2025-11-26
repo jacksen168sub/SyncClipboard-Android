ded0a72c5bf1422ae0e4359dca0232cffbc2295c
jacksen168 <liu163yong163yu@163.com>
2025-11-26 22:47:13 +0800

    chore(app): 更新应用版本号
    - 将 versionCode 从 5 更新为 6
    - 将 versionName 从 1.3.0 更新为 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.jacksen168.syncclipboard"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
