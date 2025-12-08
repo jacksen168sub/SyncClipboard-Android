1022a4afcabec706b8deda70867f8db1a1881d79
jacksen168 <liu163yong163yu@163.com>
2025-12-08 14:13:09 +0800

    chore(app): 更新应用版本号
    - 将 versionCode 从 8 更新到 9
    - 将 versionName 从 1.3.3 更新到 1.4.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.jacksen168.syncclipboard"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.3"
+        versionCode = 9
+        versionName = "1.4.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
