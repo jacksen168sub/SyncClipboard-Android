caeff04af7d0948795721e0df4c03839d33c855e
jacksen168 <liu163yong163yu@163.com>
2025-12-20 18:20:01 +0800

    chore(app): 更新应用版本号
    - 将 versionCode 从 11 更新到 12
    - 将 versionName 从 1.4.1 更新到 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.jacksen168.syncclipboard"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.1"
+        versionCode = 12
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
